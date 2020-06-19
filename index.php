0c2753ab585e3633d307cc8d2b9b59785c2a2e0f
Jonny Browning <browning@redhat.com>
2020-06-19 15:21:18 +0100

    Added colour and version for bluegreen deployment

--- index.php
+++ index.php
@@ -6,10 +6,27 @@ $birthday = date("Y-m-d H:i:s T", $birthday); // reformat date
 
 $hostname = $_ENV['HOSTNAME'];
 
+$colour = $_ENV['colour'];
+$version = $_ENV['version'];
+
 ?>
 
+<!DOCTYPE html>
+<head>
+<title>Blue-green deployment</title>
+<style>
+.message {
+    color: <?php echo $colour; ?>;
+}
+</style>
+</head>
+<body>
 <h1>Pod Details</h1>
 
 <p>This pod is called <b><?php echo $hostname; ?></b>
 
 <p>It was created at <b><?php echo $birthday; ?></b>
+
+<p class="message"><?php echo "Version $version prints this message in $colour."; ?></p>
+
+</body>
