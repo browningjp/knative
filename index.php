d74806efc62148e1ed80549aeeb61e6d59e5b28f
Jonny Browning <browning@redhat.com>
2020-06-19 10:48:41 +0100

    Added name and made some HTML

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 
-system("stat /proc/1/ | grep Change") // show container start time
+$birthday = system("stat -c='%y' /proc/1/") // container start time
+$hostname = $_ENV['HOSTNAME'];
 
 ?>
+
+<h1>Pod Details</h1>
+
+<p>This pod is called <b><?php echo $hostname; ?></b>
+
+<p>It was created at <b><?php echo $birthday; ?></b>
